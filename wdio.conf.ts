3b5578b3ffe4ffdd5d18907d7fddb36e6554561e
Mariia <mafka0903@gmail.com>
2025-04-22 10:37:38 +0300

    inst chrome

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -51,12 +51,9 @@ export const config: WebdriverIO.Config = {
   capabilities: [
     {
       browserName: "chrome",
+      webSocketUrl: false,
       "goog:chromeOptions": {
-        binary: "/usr/bin/google-chrome",
-        args: [
-          "--window-size=1920,1080",
-          `--user-data-dir=/tmp/chrome-data-${Date.now()}`,
-        ],
+        args: ["--headless", "--window-size=1920,1080"],
       },
     },
     // {
